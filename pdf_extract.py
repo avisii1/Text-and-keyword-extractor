54e4586ca5bcc53fadd5c6c97e764e1ceac06a0d
avisii1 <121296637+avisii1@users.noreply.github.com>
2023-12-27 15:31:44 +0000

    updated pdf extractor

--- pdf_extract.py
+++ pdf_extract.py
@@ -1,15 +1,9 @@
-import fitz  # PyMuPDF
+from pdfminer.high_level import extract_text
 
 
 def extract_text_from_pdf(pdf_path):
   try:
-    pdf_document = fitz.open(pdf_path)
-    text = ""
-
-    for page_num in range(pdf_document.page_count):
-      page = pdf_document[page_num]
-      text += page.get_text()
-
+    text = extract_text(pdf_path)
     return text
   except Exception as e:
     return str(e)
